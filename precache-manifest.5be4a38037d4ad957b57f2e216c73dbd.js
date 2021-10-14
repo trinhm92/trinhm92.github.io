9e4428d2095fad1160368abe9d9583766f6fd6cb
Nguyen Hoang Minh Tri <nhmt221@gmail.com>
2021-10-14 08:37:12 +0700

    Updates

---
+++ precache-manifest.5be4a38037d4ad957b57f2e216c73dbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8c669b2d839b347425261b7e085150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00213a8336bfd3002c0",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "4649b64a6b1d031e83b0",
+    "url": "/static/js/2.d5628127.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d5628127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00213a8336bfd3002c0",
+    "url": "/static/js/main.00fb237b.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "99b3275b74e22d47772c4065b1e02e80",
+    "url": "/static/media/resume.99b3275b.pdf"
+  },
+  {
+    "revision": "f9e53cde0e52bd3cc9b33eaed7780ecb",
+    "url": "/static/media/trinhm.f9e53cde.jpg"
+  }
+]);
